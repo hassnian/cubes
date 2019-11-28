11abfbb0b150711c69861214620aebf112ff6197
hassnian <hassnianxd@gmail.com>
2019-11-28 16:01:02 +0100

    functions and test of calculateIntersectionOfPlanes

--- __tests__/Space.test.js
+++ __tests__/Space.test.js
@@ -12,4 +12,116 @@ describe("space",()=>{
 		const objectInSpace = space.objectsInSpace[0]
 		expect(objectInSpace).toMatchObject(expectedObjectInSpace)
 	});
+
+	it('should return 0 when calculating the intersection in one plane where there is no intersection ',  ()  => {
+		const cubeDimensions = { x:5, y:5, z:5 }
+		const cube1Coordinates = { x:1, y:3, z:4 }
+		const cube2Coordinates = { x:10, y:4, z:4 }
+		const checkingPlane = "x"
+		const expectedIntersectionLength = 0
+
+		const space = new Space()
+		const cube1 = new Cube(cubeDimensions)
+		const cube2 = new Cube(cubeDimensions)
+
+		space.addObject(cube1,cube1Coordinates);
+		space.addObject(cube2,cube2Coordinates);
+
+		const intersectionLength = space.calculateIntersectionOfPlanes(checkingPlane)
+		expect(intersectionLength).toBe(expectedIntersectionLength)
+	});
+
+	it('should return the intersection when there is one ',  ()  => {
+		const cubeDimensions = { x:5, y:5, z:5 }
+		const cube1Coordinates = { x:1, y:3, z:4 }
+		const cube2Coordinates = { x:3, y:4, z:4 }
+		const checkingPlane = "x"
+		const expectedIntersectionLength = 3
+
+		const space = new Space()
+		const cube1 = new Cube(cubeDimensions)
+		const cube2 = new Cube(cubeDimensions)
+
+		space.addObject(cube1,cube1Coordinates);
+		space.addObject(cube2,cube2Coordinates);
+
+		const intersectionLength = space.calculateIntersectionOfPlanes(checkingPlane)
+		expect(intersectionLength).toBe(expectedIntersectionLength)
+	});
+
+	it('should return false when  there is not an intersection  ',  ()  => {
+		const firstLine = { starts : 0 ,ends: 9}
+		const secondLine = { starts : 10,ends: 100}
+		const expectedResult = false
+
+		const space = new Space()
+		const doIntersect = space.doTheCubesIntersect(firstLine,secondLine)
+
+		expect(doIntersect).toBe(expectedResult)
+	});
+	it('should return true when  there is  an intersection  ',  ()  => {
+		const firstLine = { starts : 0 ,ends: 9}
+		const secondLine = { starts : 5,ends: 100}
+		const expectedResult = true
+
+		const space = new Space()
+		const doIntersect = space.doTheCubesIntersect(firstLine,secondLine)
+
+		expect(doIntersect).toBe(expectedResult)
+	});
+
+	it('should return  0 when getting the nearest object to the origin is the first ',  ()  => {
+		const cubeDimensions = { x:5, y:5, z:5 }
+		const cube1Coordinates = { x:1, y:3, z:4 }
+		const cube2Coordinates = { x:3, y:4, z:4 }
+		const checkingPlane = "x"
+		const expectedIndex  =  0
+
+		const space = new Space()
+		const cube1 = new Cube(cubeDimensions)
+		const cube2 = new Cube(cubeDimensions)
+
+		space.addObject(cube1,cube1Coordinates);
+		space.addObject(cube2,cube2Coordinates);
+
+		const nearestToTheOrigin = space.getIndexObjectThatIsNearestToTheOrigin(checkingPlane)
+		expect(nearestToTheOrigin).toBe(expectedIndex)
+	});
+
+	it('should return 1  when getting the nearest object to the origin is the second object',  ()  => {
+		const cubeDimensions = { x:5, y:5, z:5 }
+		const cube1Coordinates = { x:1, y:3, z:4 }
+		const cube2Coordinates = { x:3, y:4, z:4 }
+		const checkingPlane = "x"
+		const expectedIndex  =  1
+
+		const space = new Space()
+		const cube1 = new Cube(cubeDimensions)
+		const cube2 = new Cube(cubeDimensions)
+
+		space.addObject(cube2,cube2Coordinates);
+		space.addObject(cube1,cube1Coordinates);
+
+		const nearestToTheOrigin = space.getIndexObjectThatIsNearestToTheOrigin(checkingPlane)
+		expect(nearestToTheOrigin).toBe(expectedIndex)
+	});
+
+	it('should return 1 if the nearest is the 0',  ()  => {
+		const expectedIndex  =  1
+		const nearest = 0
+
+		const space = new Space()
+
+		const fartherObject= space.getIndexOfFartherObjectGivingTheIndexNearest(nearest)
+		expect(fartherObject).toBe(expectedIndex)
+	});
+	it('should return 0 if the nearest is the 1',  ()  => {
+		const expectedIndex  =  0
+		const nearest = 1
+
+		const space = new Space()
+
+		const fartherObject= space.getIndexOfFartherObjectGivingTheIndexNearest(nearest)
+		expect(fartherObject).toBe(expectedIndex)
+	});
 })
--- models/Space.js
+++ models/Space.js
@@ -2,6 +2,7 @@ class Space {
 	constructor(){
 		this.objectsInSpace = []
 	}
+
 	addObject(cube,coordinates){
 		const objectInSpace = {
 			object: cube,
@@ -9,5 +10,43 @@ class Space {
 		}
 		this.objectsInSpace.push(objectInSpace)
 	}
+
+	getIndexObjectThatIsNearestToTheOrigin(plane){
+		if(this.objectsInSpace[0].coordinates[plane] <= this.objectsInSpace[1].coordinates[plane]){
+			return 0
+		}
+		return 1
+	}
+
+	getIndexOfFartherObjectGivingTheIndexNearest(nearestIndex){
+		if(nearestIndex === 0){
+			return 1
+		}
+		return 0
+	}
+
+	doTheCubesIntersect(cube1Line,cube2Line){
+		return cube1Line.ends > cube2Line.starts
+	}
+
+	calculateIntersectionOfPlanes(plane){
+		const indexOfNearest =this.getIndexObjectThatIsNearestToTheOrigin(plane)
+		const fartherObject =  this.getIndexOfFartherObjectGivingTheIndexNearest(indexOfNearest)
+
+		const cube1 = this.objectsInSpace[indexOfNearest]
+		const cube2 = this.objectsInSpace[fartherObject]
+
+		const cube1Line = { starts : cube1.coordinates[plane], ends: cube1.coordinates[plane] + cube1.object.dimensions[plane]}
+		const cube2Line = { starts : cube2.coordinates[plane], ends: cube2.coordinates[plane] + cube2.object.dimensions[plane]}
+
+		if(!this.doTheCubesIntersect(cube1Line,cube2Line)){
+			return 0
+		}
+
+		const intersection = cube1Line.ends - cube2Line.starts
+
+		return intersection
+	}
+
 }
 module.exports = { Space }
